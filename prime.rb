8f63b629138d723426e0059c2e0afeb7c962a289
C-Lion Stepansky <esstepansky@gmail.com>
2021-05-19 17:14:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(number)
   if (number <= 1)  
     return false;
  else
-    // Check from 2 to n-1
+   # Check from 2 to n-1
     for (int i=2; i<number; i++)
         if (number%i == 0)
             return false;
