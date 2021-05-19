a83bb904d00f9c1e73f1282687ddf8d5bcf6e644
C-Lion Stepansky <esstepansky@gmail.com>
2021-05-19 17:20:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 def prime?(number)
-    if (number <= 1)  
+  if (number <= 1)  
       return false;
-   elsif
+  elsif
    # Check from 2 to n-1 
    (2..num-1).to_a.all? do |possible_factors|
      if (number%i == 0)
        return false;
-   else
+  else
     return true;
   end
   
