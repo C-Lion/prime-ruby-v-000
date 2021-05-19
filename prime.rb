ce99081362e2519aa2687a337d39eb1978bd352e
C-Lion Stepansky <esstepansky@gmail.com>
2021-05-19 17:23:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(number)
   elsif
    # Check from 2 to n-1 
    (2..number-1).to_a.all? do |possible_factor|
-     if number%i == 0
+     number % i == 0
        return false;
   else
     return true;
